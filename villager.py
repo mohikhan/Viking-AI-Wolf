5ddb417036d1a82db36b3fae8a8ccc6b1fce4c0f
Mohiuddeen Khan <46248768+mohikhan@users.noreply.github.com>
2021-06-10 13:34:21 +0530

    villager role

--- villager.py
+++ villager.py
@@ -3,7 +3,8 @@ from Viking import *
 #Update function for the villager
 def villager_update(base_info,diff_data,request,player_total,player_score,myid):
 
-    logging.debug("# Villager UPDATE")
+    logging.debug("# VILLAGER UPDATE")
+    #Reduce the score of dead players
     if (request == "DAILY_INITIALIZE"):
         for i in range(player_total):
             if (base_info["statusMap"][str(i+1)] == "DEAD"):
@@ -12,26 +13,70 @@ def villager_update(base_info,diff_data,request,player_total,player_score,myid):
     
     for row in diff_data.itertuples():
         type = getattr(row,"type")
-        text = getattr(row,"text")
+        text = getattr(row,"text") 
+
+        # Voting in diff data
         if (type == "vote"):
             voter = getattr(row,"idx")
             target = getattr(row,"agent")
-            if target == myid:
-                # They voted for me!
+            # If the voter has voted for me to be a werewolf I will give him 100 hate points
+            if (target == myid):
                 logging.debug("Agent {} voted for me!".format(voter))
                 player_score[voter-1] += 100
+
+        # elif(type == "talk" and )        
+
         elif (type == "talk" and "[{:02d}]".format(myid) in text):
+            
             # they are talking about me
             source = getattr(row,"agent")
-            logging.debug("Talking about me: {}".format(text))
-            if "WEREWOLF" in text or "VOTE" in text:
-                # Are you calling me a werewolf!?
-                # Are you threateningto vote me?
-                player_score[source-1] += 10
-            else:
-                # Stop talking about me!
-                player_score[source-1] += 1
+            logging.debug("Sentence containing me: {}".format(text))
+            
+            #Reduce the hate points for the players who think I am a human
+            if "DIVINED Agent[{:02d}] HUMAN".format(myid)  in text:
+                player_score[source - 1] -=20
+            
+            #Reduce the hate points for the players who wants the bodyguard to guard me
+            elif "(GUARD Agent[{:02d}])".format(myid) in text:
+                player_score[source - 1] -= 40
 
-    # Print Current Hate list:
-    # hate = player_score.index(max(player_score)) + 1
-    # logging.debug("Hate Score: "+", ".join(str(x) for x in player_score))
+            #Increase the hate points for the agent who divines me as a werewolf 
+            #Also this agent is guaranteed werewolf because he wrongly divines me as a werewolf as I am avillager
+            elif "DIVINED Agent[{:02d}] WEREWOLF".format(myid)  in text:
+                player_score[source - 1] +=50 
+            
+            #Reduce the hate points for the players who estimate me in the human team
+            elif "ESTIMATE Agent[{:02d}] VILLAGER".format(myid) in text or "ESTIMATE Agent[{:02d}] SEER".format(myid) in text or "ESTIMATE Agent[{:02d}] MEDIUM".format(myid) in text or "ESTIMATE Agent[{:02d}] BODYGUARD".format(myid) in text:
+                player_score[source - 1] -=50 
+            
+            #Increase the hate points of the players who estimate me in the werewolf team
+            elif "ESTIMATE Agent[{:02d}] WEREWOLF".format(myid) in text or "ESTIMATE Agent[{:02d}] POSSESSED".format(myid) in text:
+                player_score[source - 1] +=50 
+
+            #Increase hate points if someone wants to vote for me or asks someone to vote for me 
+            elif "VOTE Agent[{:02d}]".format(myid) in text:
+                player_score[source - 1] +=70
+
+
+
+def villager_talk(hate,ally,ind):
+
+    logging.debug("# TALK")
+    
+    
+
+    #Taking help from allies
+    # allysentence0 = ["REQUEST Agent[{:02d}] (VOTE Agent[{:02d}])", "REQUEST ANY (GUARD Agent[{:02d}])" ,"ESTIMATE AGENT[]"]
+    
+    #sentences for the players i hate
+    hatesentence = [
+    "REQUEST ANY (VOTE Agent[{:02d}])", "ESTIMATE Agent[{:02d}] WEREWOLF","VOTE Agent[{:02d}]","REQUEST ANY (DIVINATION Agent[{:02d}])"
+    ]
+    
+    # if(ind%4 == 0):
+
+    #     return allysentence[]
+
+
+    return hatesentence[randint(0,3)].format(hate)
+
