b41f725f910b4e1d001ccf251be3a0bdc48bde3b
mohikhan <mohiuddeen20@gmail.com>
2021-06-20 21:50:49 +0530

    seer role

--- seer.py
+++ seer.py
@@ -4,6 +4,7 @@ from Viking import *
 def seer_update(base_info,diff_data,request,player_total,player_score,myid):
     
     logging.debug("# SEER UPDATE")
+    logging.debug("The request is : {}".format(request))
     if (request == "DAILY_INITIALIZE"):
         for i in range(player_total):
             #Reducing the score of dead players
@@ -25,13 +26,18 @@ def seer_update(base_info,diff_data,request,player_total,player_score,myid):
                 logging.debug("Agent {} voted for me!".format(voter))
                 player_score[voter-1] += 100
         
-        # elif(type == "divine"):
+        elif(type == "divine"):
+            source = getattr(row,"agent")
+            logging.debug("DIVINE UPDATE")
+            if("HUMAN" in text):
+                logging.debug("The result of divination is: HUMAN ")
+                player_score[source - 1] -= 80
 
-        #     source = getattr(row,"agent")
-
-        #     # playrole = 
-        #     logging.debug("The divined agent is playing the role of : {} ".format(source))
-        #     logging.debug(text)
+            elif("WEREWOLF" in text):
+                logging.debug("The result of divination is: WEREWOLF ")
+                player_score[source - 1] += 80
+            # playrole = 
+            logging.debug(text)
 
 
 
@@ -54,7 +60,7 @@ def seer_update(base_info,diff_data,request,player_total,player_score,myid):
             source = getattr(row,"agent")
             logging.debug("Sentence containing me: {}".format(text))
             
-            #Reduce the hate points for the players who think I am a human(but this time its a werewolf for sure coz im seer)
+            #Reduce the hate points for the players who think I am a human(but this time its a werewolf for sure because im the only seer)
             if "DIVINED Agent[{:02d}] HUMAN".format(myid)  in text:
                 player_score[source - 1] +=20
             
@@ -64,7 +70,7 @@ def seer_update(base_info,diff_data,request,player_total,player_score,myid):
                 player_score[source - 1] -= 40
 
             #Increase the hate points for the agent who divines me as a werewolf 
-            #Also this agent is guaranteed werewolf because he wrongly divines me as a werewolf as I am avillager
+            #Also this agent is guaranteed werewolf because he wrongly divines me as a werewolf as I am a villager
             elif "DIVINED Agent[{:02d}] WEREWOLF".format(myid)  in text:
                 player_score[source - 1] +=50 
             
@@ -94,15 +100,15 @@ def seer_talk(hate, day_no, myid, ind):
     # In this strategy I will act as a SEER and I will try to eliminate the real seer(if exists)
     # and also, I will try to remove the other players by divining them as werewolf
 
-    sentence = [
-     "REQUEST ANY (VOTE Agent[{:02d}])",
-     "ESTIMATE Agent[{:02d}] WEREWOLF",
-     "VOTE Agent[{:02d}]",
-      ]
+    # sentence = [
+    #  "REQUEST ANY (VOTE Agent[{:02d}])",
+    #  "ESTIMATE Agent[{:02d}] WEREWOLF",
+    #  "VOTE Agent[{:02d}]",
+    #   ]
 
    
         
-    strategyone_sentences = ["REQUEST ANY (VOTE Agent[{:02d}])","ESTIMATE Agent[{:02d}] WEREWOLF","VOTE Agent[{:02d}]","REQUEST ANY (DIVINATION Agent[{:02d}])"]
+    strategyone_sentences = ["REQUEST ANY (VOTE Agent[{:02d}])","ESTIMATE Agent[{:02d}] WEREWOLF","VOTE Agent[{:02d}]"]
 
     # I will come out as seer on day one and only one time
     if(day_no == 1 and ind == 0):
@@ -111,7 +117,7 @@ def seer_talk(hate, day_no, myid, ind):
         return first_sentence
 
     else:
-        return strategyone_sentences[randint(0,3)].format(hate)
+        return strategyone_sentences[randint(0,2)].format(hate)
 
  
 
