64692d2d0f4c3b54aa15d7585195c0072040b68b
mohikhan <mohiuddeen20@gmail.com>
2021-06-21 19:50:32 +0530

    seer role

--- seer.py
+++ seer.py
@@ -1,13 +1,13 @@
 from Viking import *
 
-#Update function for the seer
+# Update function for the seer
 def seer_update(base_info,diff_data,request,player_total,player_score,myid):
     
     logging.debug("# SEER UPDATE")
     logging.debug("The request is : {}".format(request))
     if (request == "DAILY_INITIALIZE"):
         for i in range(player_total):
-            #Reducing the score of dead players
+            # Reducing the score of dead players
             if (base_info["statusMap"][str(i+1)] == "DEAD"):
                 player_score[i] -= 10000
 
@@ -41,7 +41,7 @@ def seer_update(base_info,diff_data,request,player_total,player_score,myid):
 
 
 
-        #  I will increase the hate points of any other SEER becuause probably he is lying     
+        # I will increase the hate points of any other SEER becuause probably he is lying     
         elif(type =="talk" and  "SEER" in text):
             
             source = getattr(row,"agent")
@@ -60,55 +60,38 @@ def seer_update(base_info,diff_data,request,player_total,player_score,myid):
             source = getattr(row,"agent")
             logging.debug("Sentence containing me: {}".format(text))
             
-            #Reduce the hate points for the players who think I am a human(but this time its a werewolf for sure because im the only seer)
+            # Reduce the hate points for the players who think I am a human(but this time its a werewolf for sure because im the only seer)
             if "DIVINED Agent[{:02d}] HUMAN".format(myid)  in text:
                 player_score[source - 1] +=20
             
 
-            #Reduce the hate points for the players who wants the bodyguard to guard me
+            # Reduce the hate points for the players who wants the bodyguard to guard me
             elif "(GUARD Agent[{:02d}])".format(myid) in text:
                 player_score[source - 1] -= 40
 
-            #Increase the hate points for the agent who divines me as a werewolf 
-            #Also this agent is guaranteed werewolf because he wrongly divines me as a werewolf as I am a villager
+            # Increase the hate points for the agent who divines me as a werewolf 
+            # Also this agent is guaranteed werewolf because he wrongly divines me as a werewolf as I am in the villager team
             elif "DIVINED Agent[{:02d}] WEREWOLF".format(myid)  in text:
                 player_score[source - 1] +=50 
             
-            #Reduce the hate points for the players who estimate me in the human team
+            # Reduce the hate points for the players who estimate me in the human team
             elif "ESTIMATE Agent[{:02d}] VILLAGER".format(myid) in text or "ESTIMATE Agent[{:02d}] SEER".format(myid) in text or "ESTIMATE Agent[{:02d}] MEDIUM".format(myid) in text or "ESTIMATE Agent[{:02d}] BODYGUARD".format(myid) in text:
                 player_score[source - 1] -=50 
             
-            #Increase the hate points of the players who estimate me in the werewolf team
+            # Increase the hate points of the players who estimate me in the werewolf team
             elif "ESTIMATE Agent[{:02d}] WEREWOLF".format(myid) in text or "ESTIMATE Agent[{:02d}] POSSESSED".format(myid) in text:
                 player_score[source - 1] +=50 
 
-            #Increase hate points if someone wants to vote for me or asks someone to vote for me 
+            # Increase hate points if someone wants to vote for me or asks someone to vote for me 
             elif "VOTE Agent[{:02d}]".format(myid) in text:
                 player_score[source - 1] +=70
 
-     
+# talk function for the seer    
 def seer_talk(hate, day_no, myid, ind):
 
-    logging.debug("# WEREWOLF TALK")
-  
-    # I have implemented 2 strategies for the werewolf agent which will be implemented randomly for each game
-   
-  
-
-    # First strategy#######################################
-
-    # In this strategy I will act as a SEER and I will try to eliminate the real seer(if exists)
-    # and also, I will try to remove the other players by divining them as werewolf
-
-    # sentence = [
-    #  "REQUEST ANY (VOTE Agent[{:02d}])",
-    #  "ESTIMATE Agent[{:02d}] WEREWOLF",
-    #  "VOTE Agent[{:02d}]",
-    #   ]
-
-   
+    logging.debug("# SEER TALK")
         
-    strategyone_sentences = ["REQUEST ANY (VOTE Agent[{:02d}])","ESTIMATE Agent[{:02d}] WEREWOLF","VOTE Agent[{:02d}]"]
+    strategyone_sentences = ["REQUEST ANY (VOTE Agent[{:02d}])","ESTIMATE Agent[{:02d}] WEREWOLF","VOTE Agent[{:02d}]","REQUEST ANY (DIVINATION Agent[{:02d}])"]
 
     # I will come out as seer on day one and only one time
     if(day_no == 1 and ind == 0):
@@ -117,7 +100,7 @@ def seer_talk(hate, day_no, myid, ind):
         return first_sentence
 
     else:
-        return strategyone_sentences[randint(0,2)].format(hate)
+        return strategyone_sentences[randint(0,3)].format(hate)
 
  
 
